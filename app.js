af08ffa4cce80a94cb8f12d0e3e7b2770e326913
Anel Montes de Oca <105621864+anelmontes@users.noreply.github.com>
2023-11-11 10:55:21 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let nombre = "anel";
+let edad= "30"
+
+// document.write(nombre);
+
+let datos = document.getElementById("datos");
+datos.innerHTML =`
+<h1>hola</h1>
+<h2>tecnolochicas soy ${nombre}</h2>
+`;
+
+    
+
