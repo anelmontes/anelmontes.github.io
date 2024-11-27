784db7917fbfba22189dca1209d3ec4c692bdd2c
Anel Montes de Oca <105621864+anelmontes@users.noreply.github.com>
2024-11-26 20:47:41 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// console.log("Hola Mundo! desde la cosola");
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
     loop: true
 });
-
-typewriter.typeString('Desarrolladora Frontend Jr. | Programadora')
+// Comentario
+typewriter.typeString('Desarrolladora Frontend Jr.')
     .pauseFor(2500)
     .deleteAll()
     .typeString('¡Si lo puedes imaginar, lo puedes crear!')
     .pauseFor(2500)
+    // número de Caracteres que se borrarán
     .deleteChars(6)
     .typeString('<strong>programar!</strong>')
     .pauseFor(2500)
